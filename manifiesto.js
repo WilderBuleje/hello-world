f228624032b2dd33ebb8b5219c09d8fd70f77e2d
WilderBuleje <48688812+WilderBuleje@users.noreply.github.com>
2020-04-05 21:33:30 -0500

    Create manifiesto.js

---
+++ manifiesto.js
@@ -0,0 +1,16 @@
+{
+  "name": "My App",
+  "short_name": "My App",
+  "description": "What it does!",
+  "lang": "en-US",
+  "start_url": "https://wilder-buleje.shinyapps.io/Coronavirus/_w_0889ff1c/",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "C:/Users/Wilder/Desktop/Documentos/Aplicativo Movil/icono.png",
+      "sizes": "128x128",
+      "X10": 10,
+      "types": "image/png"
+    }
+  ]
+} 
