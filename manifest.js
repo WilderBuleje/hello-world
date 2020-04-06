c8d5238e9f728cf664d105f443520b244afe0162
WilderBuleje <48688812+WilderBuleje@users.noreply.github.com>
2020-04-05 23:21:05 -0500

    Update and rename manifiesto.js to manifest.js

---
+++ manifest.js
@@ -0,0 +1,14 @@
+{
+  "name": "My App",
+  "short_name": "My App",
+  "lang": "en-US",
+  "start_url": "https://raw.githubusercontent.com/WilderBuleje/hello-world/master/",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "icono.png",
+      "sizes": "314x157",
+      "types": "image/png"
+    }
+  ]
+} 
